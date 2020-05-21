fb492fc71a5b4e16c532b198119c7d4a57466189
unknown <sduarteh46@gmail.com>
2020-05-21 13:29:19 +0200

    Updates

---
+++ precache-manifest.fae9d59561a3feb65bc0dbdf5a4b36b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ac6bed2feedc5a5db04ea77eca0c1a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0715f7d72a879b817556",
+    "url": "/monsters-rolodex/static/css/main.31bb3bef.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0715f7d72a879b817556",
+    "url": "/monsters-rolodex/static/js/main.05cf4c4f.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
